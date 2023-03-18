921adb159e7d3e7398006c3dafbb8f0d374e4acd
teraiken <k_terai@aivick.co.jp>
2023-03-18 12:26:49 +0900

    pagination

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -61,6 +61,7 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                {{ $owners->links() }}
                             </div>
                         </div>
                     </section>
