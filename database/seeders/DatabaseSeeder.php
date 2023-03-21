f52d9a81e8f67b4e60289fad2e2f97abdf8599e7
teraiken <k_terai@aivick.co.jp>
2023-03-21 13:06:59 +0900

    image seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
+            ImageSeeder::class,
             ShopSeeder::class,
         ]);
     }
